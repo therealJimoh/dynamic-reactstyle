eff250a2964831a6f5b6f0687a87407e033a064e
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2021-06-06 19:31:50 +0100

    fixed the bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
-      <Navbar />
+    <Switch>
+    <Route path="/">
+      <Navbar  />
+    </Route>
+    </Switch>
     </Router>
   );
 }
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -6,7 +6,6 @@ import { Link as LinkR } from 'react-router-dom';
 export const Nav = styled.nav`
     background: #000;
     height: 80px;
-    margin-top: -80px; 
     display: flex;
     justify-content: center;
     font-size: 1rem;
